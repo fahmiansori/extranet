2256222b54aa5c4fdb468fca80be6f5bde1db19d
MF Ansori <mfahmiansori@gmail.com>
2021-05-19 17:13:01 +0700

    hotel basic

--- resources/views/hotel/index.blade.php
+++ resources/views/hotel/index.blade.php
@@ -16,6 +16,60 @@
                 </div>
 
                 <div>
+                    <div class="row">
+                        <div class="col-lg-6">
+                            <div class="text-end">
+                                Hotel Name :
+                            </div>
+                        </div>
+
+                        <div class="col-lg-6">
+                        </div>
+                    </div>
+
+                    <div class="row">
+                        <div class="col-lg-6">
+                            <div class="text-end">
+                                Star Rating :
+                            </div>
+                        </div>
+
+                        <div class="col-lg-6">
+                        </div>
+                    </div>
+
+                    <div class="row">
+                        <div class="col-lg-6">
+                            <div class="text-end">
+                                Hotel Address :
+                            </div>
+                        </div>
+
+                        <div class="col-lg-6">
+                        </div>
+                    </div>
+
+                    <div class="row">
+                        <div class="col-lg-6">
+                            <div class="text-end">
+                                Hotel Contact :
+                            </div>
+                        </div>
+
+                        <div class="col-lg-6">
+                        </div>
+                    </div>
+
+                    <div class="row">
+                        <div class="col-lg-6">
+                            <div class="text-end">
+                                Hotel Facility :
+                            </div>
+                        </div>
+
+                        <div class="col-lg-6">
+                        </div>
+                    </div>
                 </div>
             </div>
             <div class="tab-pane fade" id="tab_2_content" role="tabpanel" aria-labelledby="tab_2">
@@ -24,6 +78,31 @@
                 </div>
 
                 <div>
+                    <table class="table table-striped table-hover">
+                        <thead>
+                            <tr>
+                                <th scope="col" class="text-center">#</th>
+                                <th scope="col" class="text-center">Hotel Room ID</th>
+                                <th scope="col" class="text-center">Room Name</th>
+                                <th scope="col" class="text-center">Number of Room</th>
+                                <th scope="col" class="text-center">Room Type</th>
+                                <th scope="col" class="text-center">Action</th>
+                            </tr>
+                        </thead>
+
+                        <tbody>
+                            <tr>
+                                <th scope="row" class="text-center">1</th>
+                                <td>Mark</td>
+                                <td>Otto</td>
+                                <td>Mark</td>
+                                <td>Otto</td>
+                                <td class="text-center">
+                                    <a href="#">Edit</a>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
                 </div>
             </div>
         </div>
